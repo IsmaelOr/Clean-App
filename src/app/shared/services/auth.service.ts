1b03f0c584851306d823e44b49fd12a3bd9a750a
Ismael Ortega Estrada <ismaelortega80@gmail.com>
2023-02-21 11:54:21 -0600

    LogIn and SignIn with Google working

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -14,6 +14,7 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 })
 export class AuthService {
   userData: any;   // Guardar la información del usuario que inicio sesión.
+  isLoading: boolean = false;
 
   constructor(
     public afs: AngularFirestore,
@@ -41,12 +42,18 @@ export class AuthService {
   }
 
   async getRol(uid: string){
-    const userRef = this.afs.doc(`users/${uid}`);
-    const userInfoCifrada = await userRef.get();
     var rol = '';
-    await userInfoCifrada.forEach((data:any) => {
-      rol = data.data().rol;
-    });
+    var intentos = 0;
+    while(rol == '' && intentos < 3 ){
+      const userRef = this.afs.doc(`users/${uid}`);
+      const userInfoCifrada = userRef.get();
+      await userInfoCifrada.forEach((data:any) => {
+        if(data.data()?.rol){
+          rol = data.data().rol;
+        }
+      });
+      intentos += 1;
+    }
     return rol;
   }
 
@@ -68,7 +75,7 @@ export class AuthService {
     }).catch((error) => this.snackBar.open(error, '×', { panelClass: 'error', verticalPosition: 'top', duration: 5000 }));
 
     if(infoUsuario.user.uid){
-      const docRef = await this.afs.doc(`users/${infoUsuario.user.uid}`);
+      const docRef = this.afs.doc(`users/${infoUsuario.user.uid}`);
       const userInfo = {
         email: email,
         rol: rol
@@ -85,6 +92,47 @@ export class AuthService {
       this.router.navigate(["login"]);
     })
   }
+  
+  AuthLogin(provider: any) {
+    return this.afAuth
+      .signInWithPopup(provider)
+      .then((result) => {
+        //this.router.navigate(["sign-in"]);
+        //this.SetUserData(result.user);
+        this.snackBar.open('LogIn successfully', '×', { panelClass: 'success', verticalPosition: 'top', duration: 3000 });
+        return result;
+      })
+      .catch((error) => {
+        this.snackBar.open(error, '×', { panelClass: 'error', verticalPosition: 'top', duration: 3000 });
+      });
+    }
+
+  async GoogleAuth(rol: string) {
+    this.isLoading = true;
+    const infoUsuario : any = await this.AuthLogin(new auth.GoogleAuthProvider());
+    this.isLoading = false;
+    console.log(infoUsuario.user.uid);
+    if(infoUsuario.user.uid){
+      const userRef = this.afs.collection('users').doc(infoUsuario.user.uid);
+      var UserExist = false;
+      await userRef.get().forEach((data:any) => {
+        if(data.data()){
+          UserExist = true;
+        }
+      });
+      if(!UserExist){
+        const docRef = this.afs.doc(`users/${infoUsuario.user.uid}`);
+        const userInfo = {
+          email: infoUsuario.user.email,
+          rol: rol
+        }
+        docRef.set(userInfo);
+      }
+      this.router.navigate(["home"]);
+    }
+  }
+
+
 
 
 }
